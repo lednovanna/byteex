4e425b56289da97bd5431c10ccaea58ad09f01b2
lednovanna <lelagency@gmail.com>
2024-09-27 23:39:58 +0100

    new sectionCollage added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import HeroSection from './components/HeroSection';
 import DescriptionBenefits from './components/DesciptionBenefits';
 import YourBest from './components/YourBest';
 import SectionComfort from './components/SectionComfort';
+import SectionCollage from './components/SectionCollage';
 import './App.css';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
      <DescriptionBenefits/>
      <YourBest/>
      <SectionComfort/>
+     <SectionCollage/>
     </div>
   );
 }
---
+++ src/components/SectionCollage.js
@@ -0,0 +1,46 @@
+import React from "react";
+import './SectionCollage.css';
+
+function SectionCollage() {
+    return (
+        <div className="sectionCollage">
+            
+            
+  <img src="/assets/img/img4.jpg" alt="Collage" />
+  <img src="/assets/img/img5.jpg" alt="Collage" />
+  <img src="/assets/img/img6.jpg" alt="Collage" />
+  <img src="/assets/img/img7.png" alt="Collage" />
+  <img src="/assets/img/img8.png" alt="Collage" />
+  <img src="/assets/img/img9.png" alt="Collage" />
+  <img src="/assets/img/img10.png" alt="Collage" />
+  <img src="/assets/img/img11.png" alt="Collage" />
+  <img src="/assets/img/img12.png" alt="Collage" />
+  <img src="/assets/img/img13.png" alt="Collage" />
+  <img src="/assets/img/img14.png" alt="Collage" />
+  <img src="/assets/img/img15.png" alt="Collage" />
+  <img src="/assets/img/img16.png" alt="Collage" />
+  <img src="/assets/img/img17.png" alt="Collage" />
+  <img src="/assets/img/img18.png" alt="Collage" />
+  <img src="/assets/img/img19.png" alt="Collage" />
+  <img src="/assets/img/img20.png" alt="Collage" />
+  <img src="/assets/img/img21.jpg" alt="Collage" />
+  <img src="/assets/img/img22.jpg" alt="Collage" />
+  <img src="/assets/img/img21.jpg" alt="Collage" />
+  <img src="/assets/img/img24.jpg" alt="Collage" />
+  <img src="/assets/img/img25.jpg" alt="Collage" />
+  <img src="/assets/img/img4.jpg" alt="Collage" />
+  <img src="/assets/img/img5.jpg" alt="Collage" />
+  <img src="/assets/img/img6.jpg" alt="Collage" />
+  <img src="/assets/img/img7.png" alt="Collage" />
+  <img src="/assets/img/img8.png" alt="Collage" />
+  <img src="/assets/img/img9.png" alt="Collage" />
+  <img src="/assets/img/img10.png" alt="Collage" />
+  <img src="/assets/img/img11.png" alt="Collage" />
+  <img src="/assets/img/img12.png" alt="Collage" />
+  <img src="/assets/img/img13.png" alt="Collage" />
+  
+        </div>
+    )
+}
+
+export default SectionCollage;
